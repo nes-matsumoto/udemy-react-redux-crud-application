b40061841dbf83bdd82ac38219d87dfbab4e132c
1134142020804 <1134142020804@nsl.ad.nec.co.jp>
2019-11-21 16:49:53 +0900

    learn jsx syntax.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 
 
+//JSXでの表現
 function App() {
-  return <div><h1>hello,world!!</h1></div>;
+  const greeting = "Nice!Greeting!"
+const dom = <h1 className="foo">{greeting}</h1>
+
+return(
+  <React.Fragment>
+    <label htmlFor=""bar>bar</label>
+    <input type="text" onChange={() => {console.log("I am clicked")}}/>;
+  </React.Fragment>
+)
 }
+
+//Javascriptでの表現
+//function App() {
+//  return React.createElement("div",null,"Matsumoto Kazuma!!");
+//}
 
 export default App;
