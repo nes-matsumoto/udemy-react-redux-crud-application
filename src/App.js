53f50ffbfcd4449c16558ada3c1bbf1c5a27b465
1134142020804 <1134142020804@nsl.ad.nec.co.jp>
2019-11-22 10:09:41 +0900

    studing props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 //JSXでの表現
-function App() {
+/*function App() {
   const greeting = "Nice!Greeting!"
 const dom = <h1 className="foo">{greeting}</h1>
 
@@ -13,10 +13,39 @@ return(
   </React.Fragment>
 )
 }
+*/
+const App = () => {
+
+  const profiles = [
+    {name:"kazuma",age:10},
+    {name:"taro",age:10}
+  ]
+
+  return(
+    <div>
+      {
+        profiles.map((profile) => {
+          return <user name={"profile.name"} age={profile.age}/>
+
+        }) 
+      }
+
+      <user name={"kazuma"} age={39}/>
+
+      <user name={"Taro"} age={19}/>
+    </div>
+  )
+}
+
+
+const user = (props) => {
+  return <div>Hello!!{props.name} age:{props.age}</div>
+}
+
 
 //Javascriptでの表現
-//function App() {
-//  return React.createElement("div",null,"Matsumoto Kazuma!!");
-//}
-
+/*function App() {
+  return React.createElement("div",null,"Matsumoto Kazuma!!");
+}
+*/
 export default App;
